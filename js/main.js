a34ad94b78a45b4fd5f87222c168f0f539bb625a
zhimmat <zeyad.himmat@live.com>
2016-04-01 16:28:32 -0400

    got rectangle and diamond to work

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ var $box = $('.box');
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
+
 $btnShowHide.on('click', function () {
   $box.toggleClass('showbox');
+});
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('btn-slide');
+});
+
+$btnMove.on('click', function () {
+  $panel.toggleClass('collapse-panel');
 });
